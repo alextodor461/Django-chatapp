0ac5c6b34fec92ad63f28581a775df07e8e90383
Alexandru-Andrei Todor <at@interap.de>
2022-07-12 11:43:02 +0200

    end design register/login

--- django_chat_app/register/views.py
+++ django_chat_app/register/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
+from django.db import IntegrityError
 
 
 def registerUser(request):
     if request.method == 'POST':
-        user = User.objects.create_user(first_name = request.POST.get('firstname'),  username = request.POST.get('username'), email = request.POST.get('email'), password = request.POST.get('password'))
-        
-        return HttpResponseRedirect('/login/')
+        try:
+            user = User.objects.create_user(first_name = request.POST.get('firstname'),  username = request.POST.get('username'), email = request.POST.get('email'), password = request.POST.get('password'))
+            return HttpResponseRedirect('/login/')
+        except IntegrityError:
+            return HttpResponseRedirect('/register?error=Bitte verwende einen anderen Username')
+        except Exception as e:
+            print(e)
+            return HttpResponseRedirect('/register?error=Ein Unbekannter Fehler ist aufgetreten')
+                      
     return render(request, 'register/index.html')
     
